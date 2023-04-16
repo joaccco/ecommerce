b61cb6bba4013146f4ab619ea4e43f357a98dc1f
joaccco <joaccofranc02@icloud.com>
2023-04-16 00:20:28 -0300

    Navigation

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,7 @@
 export default function AboutPage () {
     return (
-        <h2>Nosotros</h2>
+        <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
+            Nosotros
+        </h2>
     )
-
 }
